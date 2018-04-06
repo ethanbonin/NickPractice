f31e5842254b9ac09cb4b85b1592a5f171b51a52
Christopher Ethan Bonin <ethanbonin@gmail.com>
2018-04-06 14:04:24 -0600

    Initial Commit

---
+++ nickproject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nickproject
+//
+//  Created by Ethan Bonin on 4/6/18.
+//  Copyright © 2018 Ethan Bonin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
